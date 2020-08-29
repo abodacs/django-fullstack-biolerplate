86b28a8784795db8c306809cc0bb96ef1e8e510b
abdullahmm <abdullahabdelrhim@gmail.com>
2020-08-29 22:56:59 +0200

    fix: add LocaleMiddleware

--- backend/onlineBenevolent/settings/base.py
+++ backend/onlineBenevolent/settings/base.py
@@ -63,6 +63,7 @@ AUTH_USER_MODEL = "users.User"
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
+    "django.middleware.locale.LocaleMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
     "django.contrib.auth.middleware.AuthenticationMiddleware",
